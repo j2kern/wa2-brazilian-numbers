a3128ee4cc1a788c6b3091bffe36690e38d15bf2
Juliet <59781632+j2kern@users.noreply.github.com>
2020-01-17 17:19:45 -0500

    Update wa2.cs

--- wa2.cs
+++ wa2.cs
@@ -12,53 +12,54 @@ namespace c
             Write("Enter a positive integer: ");
             int n = int.Parse (ReadLine( ));
             
-				for(int b = n - 1 ; b > 1 ; b--) //finds first remainder
+		for(int b = n - 1 ; b > 1 ; b--) //finds first remainder
+		{
+			bool digitsSame = true;
+			bool notDone = true;
+			int m = n;
+			int counter = 0;
+			counter ++;
+			int r = n % b;
+					
+			while(notDone && digitsSame) //removes rightmost digit and finds new remainder
+			{
+				m = m / b;
+				int newr = m % b;
+					
+				if(newr == r)
 				{
-					bool digitsSame = true;
-					bool notDone = true;
-					int m = n;
-					int counter = 0;
 					counter ++;
-					int r = n % b;
+				}
+				else if(m == 0)
+				{
+					notDone = false;
+				}
+				else
+				{
+					digitsSame = false;
+				}
+			}
 					
-					while(notDone && digitsSame) //removes rightmost digit and finds new remainder
-					{
-					m = m / b;
-					int newr = m % b;
-					
-						if(newr == r)
+			if(digitsSame && counter > 1) //conditions allow remainders to be printed counter # of times
+				{
+					Write($"{n} " + "in base 10 is ");
+						
+					for(int i = 0; i < counter; i++)
+					{ 
+						if(i == counter - 1)
 						{
-						counter ++;
-						}
-						else if(m == 0)
-						{
-							notDone = false;
+							Write(r + " ");
 						}
 						else
 						{
-							digitsSame = false;
+							Write( r + ",");
 						}
 					}
-					
-					if(digitsSame && counter > 1) //conditions allow remainders to be printed counter # of times
-					{
-						Write($"{n} " + "in base 10 is ");
+					WriteLine("in base" + $" {b}");
 						
-						for(int i = 0; i < counter; i++)
-						{ 
-							if(i == counter - 1)
-								{
-								Write(r + " ");
-								}
-							else
-								{
-								Write( r + ",");
-								}
-						}
-						WriteLine("in base" + $" {b}");
-						
-					}
 				}
-        }
+			}
+	}
     }
 }
+
